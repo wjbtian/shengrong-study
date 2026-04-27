55fee42d4c12d4a0be5b56cd9e7ff5f9ae158e0e
wjbtian <304366133@qq.com>
2026-04-27 08:19:05 +0800

    fix: Vue Router 改为 hash 模式，兼容静态服务器

--- vue-app/src/router/index.js
+++ vue-app/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import DiaryView from '../views/DiaryView.vue'
 import ChineseView from '../views/ChineseView.vue'
@@ -22,7 +22,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
